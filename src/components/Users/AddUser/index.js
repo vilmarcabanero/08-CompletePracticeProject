4bc4a9c3d83f75221def89c2fb9f6a8ded183ade
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-05-21 18:57:11 +0800

    89. Adding a "User" Component

--- src/components/Users/AddUser/index.js
+++ src/components/Users/AddUser/index.js
@@ -1,26 +1,23 @@
 import React from 'react'
 import * as S from './index.style'
-import * as Mui from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(1),
-      width: '25ch',
-    },
-  },
-}));
+import StylesProvider from "@material-ui/styles/StylesProvider";
 
 const Index = () => {
-  const classes = useStyles();
-  return (
-    <form className={classes.root} noValidate autoComplete="off">
-    <Mui.TextField id="standard-basic" label="Standard" />
-    <Mui.TextField id="filled-basic" label="Filled" variant="filled" />
-    <Mui.TextField id="outlined-basic" label="Outlined" variant="outlined" />
-  </form>
-  )
+	const addUserHandler = e => {
+		e.preventDefault()
+	}
+
+	return (
+		<StylesProvider injectFirst>
+			<S.Container>
+				<S.Form onSubmit={addUserHandler}>
+					<S.Input label='Username' />
+					<S.Input type='number' label='Age (Years)' />
+					<S.Button variant="contained" color="primary" type='submit'>Add User</S.Button>
+				</S.Form>
+			</S.Container>
+		</StylesProvider>
+	)
 }
 
 export default Index
---
+++ src/components/Users/AddUser/index.style.js
@@ -0,0 +1,23 @@
+import * as Mui from '@material-ui/core'
+import styled from 'styled-components'
+
+export const Form = styled.form`
+	width: '100%';
+`
+
+export const Container = styled.div`
+	width: 28rem;
+	margin-left: auto;
+	margin-right: auto;
+	margin-top: 5rem;
+	padding: 1rem;
+`
+
+export const Input = styled(Mui.TextField)`
+	margin-top: 1rem;
+	width: 100%;
+`
+export const Button = styled(Mui.Button)`
+	margin-top: 2rem;
+	width: 100%;
+`
